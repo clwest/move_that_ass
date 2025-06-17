1df241b346b82a6717c26fcb41d2feae0422abca
Chris West <westremarketing@gmail.com>
2025-06-17 17:05:55 -0600

    Add auth permissions and docstrings to viewsets

--- backend/movement/views.py
+++ backend/movement/views.py
@@ -1,14 +1,21 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import MovementChallenge, MovementSession
 from .serializers import MovementChallengeSerializer, MovementSessionSerializer
 
 
 class MovementChallengeViewSet(viewsets.ModelViewSet):
+    """API for creating and updating movement challenges."""
+
     queryset = MovementChallenge.objects.all()
     serializer_class = MovementChallengeSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class MovementSessionViewSet(viewsets.ModelViewSet):
+    """Track a user's individual movement sessions."""
+
     queryset = MovementSession.objects.all()
     serializer_class = MovementSessionSerializer
+    permission_classes = [IsAuthenticated]
